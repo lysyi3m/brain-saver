b558601a92b42c85288ef64aa612843b369f8594
Emil Kashkevich <emil.kashkevich@gmail.com>
2015-11-17 01:12:31 +0200

    Fix ga script location

--- options/options.js
+++ options/options.js
@@ -30,3 +30,17 @@ document.getElementById('form-save').addEventListener('click', (e) => {
     saveOptions({phrases: []});
   }
 });
+
+// Google Analytics stuff
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-70266101-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
